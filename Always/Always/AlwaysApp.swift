65407856522276b82edf6148b8f7c28fe7c1624b
Isuru Nanayakkara <isuru.nan@gmail.com>
2022-05-01 12:02:46 +0530

    Black Xcode project

---
+++ Always/Always/AlwaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlwaysApp.swift
+//  Always
+//
+//  Created by Isuru Nanayakkara on 2022-05-01.
+//
+
+import SwiftUI
+
+@main
+struct AlwaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
