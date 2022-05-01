cf2b518de75a5b855e850d1d435c7bcd83e2ece3
Isuru Nanayakkara <isuru.nan@gmail.com>
2022-05-01 12:22:27 +0530

    Created model classes

---
+++ Always/Always/Models/Breakdown.swift
@@ -0,0 +1,15 @@
+//
+//  Breakdown.swift
+//  Always
+//
+//  Created by Isuru Nanayakkara on 2022-05-01.
+//
+
+import Foundation
+
+struct Breakdown {
+    private let id: String
+    private let startTime: Date
+    private let endTime: Date
+    private let title: String
+}
---
+++ Always/Always/Models/BreakdownGroup.swift
@@ -0,0 +1,12 @@
+//
+//  BreakdownGroup.swift
+//  Always
+//
+//  Created by Isuru Nanayakkara on 2022-05-01.
+//
+
+import Foundation
+
+struct BreakdownGroup {
+    private let breakdowns: [Breakdown]
+}
